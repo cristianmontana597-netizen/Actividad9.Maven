f9543b283154be7150f673500ab245e0dfcf8f22
general <general@PGCITY1515-512.poligran.edu.co>
2026-04-20 17:12:36 -0500

    interactuar boton borrar para borrar texto de la caja de texto

--- actividad9/src/main/java/co/edu/poli/actividad9/PrimaryController.java
+++ actividad9/src/main/java/co/edu/poli/actividad9/PrimaryController.java
@@ -1,12 +1,18 @@
 package co.edu.poli.actividad9;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
 public class PrimaryController {
 
+    // Esto vincula el TextArea de Scene Builder con esta variable
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextArea txtArea;
+
+    // Este es el método que se ejecutará al presionar el botón
+    @FXML
+    private void limpiarCuadro() {
+        txtArea.clear(); // Borra todo lo que haya escrito el usuario
+        System.out.println("Se ha limpiado el cuadro de texto.");
     }
 }
